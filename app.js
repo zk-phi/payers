1ec02b8c978b5cc2825d7ba3ce87ecea8ced6a8b
p <_>
2017-08-01 18:45:31 +0900

    Record category and timestamp

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var TITLE = "お金の貸し借り💰";
 var MAPS_API_KEY = PropertiesService.getScriptProperties().getProperty("MAPS_API_KEY");
+var CATEGORIES = [
+    { key: 1, label: "ランチ" },
+    { key: 2, label: "ディナー" },
+    { key: 3, label: "おやつ" },
+    { key: 4, label: "遊び" },
+    { key: 5, label: "買い物" },
+    { key: 6, label: "旅費" },
+    { key: 0, label: "他" },
+];
 
 function doGet () {
     var template = HtmlService.createTemplateFromFile("index.html");
@@ -15,7 +24,8 @@ function doFetchLastState () {
     return {
         users: dbFind("kvs", "users") || ["user1", "user2"],
         amountDiff: dbFind("kvs", "amountDiff") || 0,
-        log: dbFind("kvs", "log") || []
+        log: dbFind("kvs", "log") || [],
+        categories: CATEGORIES
     };
 }
 
@@ -24,10 +34,10 @@ function doSaveUsernames (users) {
     return users;
 }
 
-function doPay (payer, amount, location) {
+function doPay (payer, amount, location, category, timestamp) {
     var log = dbFind("kvs", "log") || [];
     var diff = dbFind("kvs", "amountDiff") || 0;
-    log.unshift({ payer: payer, amount: amount, location: location });
+    log.unshift({ payer: payer, amount: amount, location: location, category: category, timestamp: timestamp });
     diff = payer ? diff + amount : diff - amount;
     dbSet("kvs", "log", log);
     dbSet("kvs", "amountDiff", diff);
